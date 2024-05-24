e441e01699c12e39a2e55668cc0af78ac15e63b1
plhrtr <pl.hrtr@gmail.com>
2024-05-24 14:06:07 +0200

    test

--- bookingSystemFrontend/src/main.tsx
+++ bookingSystemFrontend/src/main.tsx
@@ -1,17 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools></ReactQueryDevtools>
-    </QueryClientProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
